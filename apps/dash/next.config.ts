7a8693b20c47e3955f7f5e67694d2fb671cb11ff
iRazz <hi@irazz.lol>
2026-05-01 20:03:16 +0300

    update packages & skills

--- apps/dash/next.config.ts
+++ apps/dash/next.config.ts
@@ -1,3 +1,4 @@
+import withBundleAnalyzer from "@next/bundle-analyzer";
 import { loadEnv } from "@uptimekit/config/env";
 import type { NextConfig } from "next";
 
@@ -26,4 +27,6 @@ const nextConfig: NextConfig = {
 	},
 };
 
-export default nextConfig;
+export default withBundleAnalyzer({
+	enabled: process.env.ANALYZE === "true",
+})(nextConfig);
